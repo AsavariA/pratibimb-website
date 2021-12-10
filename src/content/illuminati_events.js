2855393e6ce6e74f3ebea02db7bd80cfd9a44336
chaitravi-ce <chaitravi007@gmail.com>
2021-12-10 18:57:44 +0530

    Added Illuminati 2021

--- src/content/illuminati_events.js
+++ src/content/illuminati_events.js
@@ -1,22 +1,25 @@
+import comic from "../assets/illuminati/comic-strip.png"
+import jingles from "../assets/illuminati/jingles.png"
+
 const illuminati_events = [
   {
-    name: "",
-    poster: "",
-    overview: "",
-    results: [], //list of branch rankings like ['extc', 'civil', ...]
+    name: "Comic Strip",
+    poster: comic,
+    overview: "sketch story",
+    results: ["Tronics", "CS", "IT"], //list of branch rankings like ['extc', 'civil', ...]
   },
   {
-    name: "",
-    poster: "",
-    overview: "",
-    results: [], //list of branch rankings like ['extc', 'civil', ...]
+    name: "Jingles",
+    poster: jingles,
+    overview: "poster poster poster",
+    results: ["Tronics", "CS", "IT"], //list of branch rankings like ['extc', 'civil', ...]
   },
-  {
-    name: "",
-    poster: "",
-    overview: "",
-    results: [], //list of branch rankings like ['extc', 'civil', ...]
-  },
+  // {
+  //   name: "",
+  //   poster: "",
+  //   overview: "",
+  //   results: [], //list of branch rankings like ['extc', 'civil', ...]
+  // },
 ];
 
 export default illuminati_events;
